32eb45e38ed513a5b7e66e820b53cbcc5b76ce00
David Riddlebarger <Driddlebarger@gmail.com>
2017-10-25 23:02:08 -0700

    Add authentication to locations CRUD actions

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,12 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  get 'submit' => 'pages#submit'
+  devise_scope :user do
+  	get '/login', to: 'devise/sessions#new'
+  	get '/logout', to: 'devise/sessions#destroy', as: :signout
+  end
 
+  get 'submit' => 'pages#submit'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
