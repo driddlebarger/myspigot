c5a6b822217f5c83e01cd805a28049ef8e4939e8
David Riddlebarger <Driddlebarger@gmail.com>
2017-11-04 14:27:58 -0700

    fixed sendgrid syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'https://waterfinder.herokuapp.com/' }
-  config.action_mailer.delivery_method = :smpt
+  config.action_mailer.delivery_method = :smtp
 
       ActionMailer::Base.smtp_settings = {
         :user_name => ENV['SENDGRID_USERNAME'],
