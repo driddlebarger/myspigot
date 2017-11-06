136c455c75594ae680726892dcdaa3d1d679c3ca
David Riddlebarger <Driddlebarger@gmail.com>
2017-11-06 07:37:46 -0800

    Submission controller add flash messages

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -6,5 +6,12 @@ class SubmissionsController < ApplicationController
   def create
     @submission = Submission.new(params[:submission])
     @submission.request = request
+    if @contact.deliver
+    	flash.now[:error] = nil
+    	redirect_to root_path, notice: "Thanks for your submission!"
+    else
+    	flash.now[:error] = 'Submission could not be completed.'
+    	render :new
+    end
   end
 end
