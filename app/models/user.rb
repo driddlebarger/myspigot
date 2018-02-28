b80a5b00f9f959e956e7e2010aff5417051ad7e1
David Riddlebarger <Driddlebarger@gmail.com>
2018-02-27 21:32:13 -0800

    Show page styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, 
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
-
   acts_as_voter
-  
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
