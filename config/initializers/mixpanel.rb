039380271d6109a2d4044168ce14557f979bea38
David Riddlebarger <Driddlebarger@gmail.com>
2018-03-02 11:52:48 -0800

    Mixpanel initializer

--- config/initializers/mixpanel.rb
+++ config/initializers/mixpanel.rb
@@ -1,9 +1,10 @@
 require 'mixpanel-ruby'
 
-if Rails.env == "development"
-  $tracker = Mixpanel::Tracker.new('15053396e356fe89af9c2b17b2633a3a')
-end
+$tracker = Mixpanel::Tracker.new('15053396e356fe89af9c2b17b2633a3a')
 
-if Rails.env == "production"
-  $tracker = Mixpanel::Tracker.new('15053396e356fe89af9c2b17b2633a3a')
+if Rails.env.development? 
+  #silence local SSL errors
+  Mixpanel.config_http do |http|
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  end
 end
