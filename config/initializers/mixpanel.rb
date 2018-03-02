034787ec418de29339561c617f0b26fc52339b8c
David Riddlebarger <Driddlebarger@gmail.com>
2018-03-02 11:39:36 -0800

    mixpanel token test

--- config/initializers/mixpanel.rb
+++ config/initializers/mixpanel.rb
@@ -5,5 +5,5 @@ if Rails.env == "development"
 end
 
 if Rails.env == "production"
-  $tracker = Mixpanel::Tracker.new(ENV['MIXPANEL_PROD_TOKEN'])
+  $tracker = Mixpanel::Tracker.new('15053396e356fe89af9c2b17b2633a3a')
 end
