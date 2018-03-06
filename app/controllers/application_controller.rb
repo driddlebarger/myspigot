43c69b5a9e9fb6cebabde47785c17db8fc7665d4
David Riddlebarger <Driddlebarger@gmail.com>
2018-03-05 19:48:15 -0800

    Devise store location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :store_user_location!, if: :storable_location?
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
-  # this method is not being used
-  #def auth_user
-  #  redirect_to new_user_registration_url unless user_signed_in?
-  #  flash[:notice] = "Please sign in to continue."
-  #end
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
+
 
   protected
 
@@ -17,9 +18,14 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
   end
 
-  # use this as a fallback
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || '/submit'
+    stored_location_for(resource) || root_path
+  end
+
+  private
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
   end
 
 
